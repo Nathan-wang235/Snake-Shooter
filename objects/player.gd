b315601171735a6d4a50b08f1d2db6070fa3b70a
Nathan-wang235 <25644@burnside.school.nz>
2026-08-04 10:20:55 +1200

    refined enemies spawner

--- objects/player.gd
+++ objects/player.gd
@@ -325,7 +325,7 @@ func change_weapon():
 	
 	# Step 2. Place new weapon model in container
 	
-	var weapon_model = weapon.model.instwantiate()
+	var weapon_model = weapon.model.instantiate()
 	container.add_child(weapon_model)
 	
 	weapon_model.position = weapon.position
--- scenes/enemies_spawner.gd
+++ scenes/enemies_spawner.gd
@@ -1,11 +1,34 @@
 extends Node3D
 
-@export var enemy_prefab : PackedScene
-@export var player_node : CharacterBody3D
+class_name EnemySpawner
 
+@export var enemy_prefab: PackedScene
+@export var player_node: CharacterBody3D
 
-func _on_timer_timeout() -> void:
-	var enemy = enemy_prefab.instantiate()
-	add_child(enemy)
-	enemy.player = player_node
-	enemy.set_random_place()
+func _ready():
+	spawn($spawn_points/Start_01, 5)
+
+func spawn(spawn_point: Marker3D, count: int):
+
+	print("spawn_point =", spawn_point)
+	print("enemy_prefab =", enemy_prefab)
+
+	for i in range(count):
+
+		var enemy = enemy_prefab.instantiate()
+
+		print("enemy =", enemy)
+
+		if spawn_point == null:
+			push_error("spawn_point is NULL!")
+			return
+
+		if enemy == null:
+			push_error("enemy is NULL!")
+			return
+
+		add_child(enemy)
+
+		enemy.global_position = spawn_point.global_position
+
+		enemy.player = player_node
